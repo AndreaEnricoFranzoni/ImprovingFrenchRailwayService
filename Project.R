186023eae30dfbee10f4e5750d86993912dbf432
NicolaFrancescon <nicola.francescon00@gmail.com>
2023-10-27 18:31:30 +0200

    Update Project.R

--- Project.R
+++ Project.R
@@ -1,3 +1,4 @@
+graphics.off()
 data=read_excel('trains_update_2610.xlsx')
 n=dim(data)[1]
 n
@@ -15,7 +16,7 @@ num_tratte = length(unique(data$route))
 cols = (length(unique(data$year)))*(length(unique(data$month)))
 dati_nuovi = matrix(nrow=num_tratte, ncol = cols)
 
-r=(unique(data$route))
+r = (unique(data$route))
 for (k in 1:num_tratte) {
   for (j in years) {
     for ( i in months){
@@ -26,27 +27,27 @@ for (k in 1:num_tratte) {
   dati_nuovi[k,48]=r[k]
 }
 
-dati_nuovi=data.frame(dati_nuovi)
+dati_nuovi = data.frame(dati_nuovi)
 
 fun = t(as.matrix(dati_nuovi[1:112,1:47]))
-time=1:47
+time = 1:47
 
 x11()
 matplot(time,fun, type='l')
 
 
-f_data <- fData(time, t(fun))
+f_data = fData(time, t(fun))
 x11()
 plot(f_data)
 
-band_depth <- BD(Data = f_data) #in input va un FD, e ritorna un vector con la band depth di ogni elemento
-modified_band_depth <- MBD(Data = f_data) #in input va un FD, e ritorna un vector con la modified band depth di ogni elemento
+band_depth = BD(Data = f_data) #in input va un FD, e ritorna un vector con la band depth di ogni elemento
+modified_band_depth = MBD(Data = f_data) #in input va un FD, e ritorna un vector con la modified band depth di ogni elemento
 
-median_curve <- median_fData(fData = f_data, type = "MBD") # still an fData object: la mediana è la curva con la depth più alta
+median_curve = median_fData(fData = f_data, type = "MBD") # still an fData object: la mediana è la curva con la depth più alta
 
 x11()
 plot(f_data) 
-lines(time,median_curve$values, lwd=10)
+lines(time,median_curve$values, lwd=2)
 #lines(time,modified_band_depth, lwd=10, col='black')
 
 EI(f_data) #per ogni curva, calcola l'Epigraph Index 
