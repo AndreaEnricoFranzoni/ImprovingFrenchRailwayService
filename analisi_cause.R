c81926df47f0609e82c2d9c1538b71c0aa0b5314
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2023-12-12 20:06:53 +0100

    cause

--- analisi_cause.R
+++ analisi_cause.R
@@ -1,6 +1,7 @@
 library(readxl)
-install.packages('compositions')
+install.packages('compositions') #libreria che serve per trattare compositional data
 library(compositions)
+library(robustbase)
 
 finestra_grafica = function(t){
   if (t==0)
@@ -24,15 +25,27 @@ i_2018=which(data$year==2018)
 indexes=list(i_2015, i_2016, i_2017, i_2018)
 nomi_cause=c('External','Rail infrastructure','Traffic management',
              'Rolling stock','Station management','Travelers') #se mettiamo questi come nomi delle colonne i plot successivi vengono un pochino più carini
+cause=data[,14:19]
+colnames(cause)=nomi_cause
 
 #prendo le cause del 2018
-cause_2018=data[i_2018,14:19]
-#le trasformo
-cause_2018_transformed=acomp(cause_2018)
-finestra_grafica(t)
-plot(cause_2018_transformed)
+cause_2018=cause[i_2018,]
+cause_2018_comp=acomp(cause_2018) #è semplicemente un oggetto di classe compositional data, i dati non sono trasformati
+#Isometric log ratio transform
+cause_2018_transformed=ilr(cause_2018) #questi sono i dati trasformati: è un oggetto da S^6 in R^5
 
-pc <- princomp(acomp(cause_2015))
+finestra_grafica(t)
+plot(cause_2018_comp)         #plot dei simplessi
+
+finestra_grafica(t)
+plot(cause_2018_transformed) #scatter dei dati trasformati: COME INTERPRETARLI??
+
+finestra_grafica(t)
+boxplot(cause_2018_comp)
+
+
+
+pc <- princomp(acomp(cause_2018))
 pc
 summary(pc)
 finestra_grafica(t)
