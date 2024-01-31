8a58baed7bde8e58faafe613cddb4cdebb803938
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-01-31 21:35:55 +0100

    Model for delay at arrival vs causes + sensitivity analysis

--- analisi_cause.R
+++ analisi_cause.R
@@ -73,6 +73,7 @@ cause_dicotomiche_2018_trans = ilr(cause_dicotomiche_2018_comp)
 
 finestra_grafica(t)
 plot(1:n_2018,cause_dicotomiche_2018_trans)
+#sono tutte negative: le interne sono preponderanti rispetto alle esterne
 
 
 
@@ -80,8 +81,17 @@ plot(1:n_2018,cause_dicotomiche_2018_trans)
 #come funziona: è la PCA normale su una trasformazione specifica:
 #io devo passare un oggetto acomp
 # di default usa la clr
-pc <- princomp((acomp(cause_2018))) #usa la clr: ho 6 scores perchè sono in R6
+#pc <- princomp((acomp(cause_2018))) #usa la clr: ho 6 scores perchè sono in R6
 #pc = princomp(ilr(acomp(cause_2018)))  #sto mettendo la ilr: ho 5 scores perchè fatto in R5
+#pc
+#summary(pc)
+#finestra_grafica(t)
+#plot(pc)
+#pc$Loadings
+#pc$scores
+#biplot(pc)  
+
+pc = princomp(acomp(cause_dicotomiche_2018))
 pc
 summary(pc)
 finestra_grafica(t)
@@ -89,10 +99,6 @@ plot(pc)
 pc$Loadings
 pc$scores
 biplot(pc)  
-
-pc = princomp(acomp(cause_dicotomiche_2018))
-summary(pc)
-
 
 # Per Andrea o chi lo farà: 
 # Partire da regressione di avg_delay_late_at_arrival con tutti i loadings insieme e valutare se ci sono coefficienti non significativi.
@@ -102,4 +108,153 @@ summary(pc)
 # è giusto farlo sul 2018 perché sono le più recenti e dunque possiamo dire alla compagnia di intervenire se ci sono risultati
 # significativi
 
+response = data$avg_delay_late_on_arrival[i_2018]
 
+#first model: about only the two
+clr_internal = pc$scores[,1] 
+clr_external = pc$scores[,2]
+
+#linear model
+fit_1 = lm( response ~ clr_internal + clr_external )
+summary(fit_1)
+shapiro.test(fit_1$residuals)
+#considero i residui gaussiani: il test globale è esatto: tutti i coefficienti nulli
+#not a good thing
+
+#I take into account the 4 internal causes separately, putting together the other
+#two: traverls and external
+
+cause_2018_dati = data.frame(cause_2018$`Rail infrastructure`,
+                             cause_2018$`Traffic management`,
+                             cause_2018$`Rolling stock`,
+                             cause_2018$`Station management`,
+                             cause_2018$External+cause_2018$Travelers
+                             )
+colnames(cause_2018_dati) = c('Rail infrastructure','Traffic management','Rolling stock', 'Station management','Externals')
+cause_2018_dati_comp = acomp(cause_2018_dati)
+
+finestra_grafica(t)
+plot(cause_2018_dati_comp)  
+#it makes sense to try to understand how that works
+
+trasf_cause = ilr(cause_2018_dati)
+#what I have: I am in R4, having the log of the ratio between all the internal over
+# the externals: take a look to better see it
+
+x1 = trasf_cause[,1]
+x2 = trasf_cause[,2]
+x3 = trasf_cause[,3]
+x4 = trasf_cause[,4]
+
+fit_2 = lm( response ~ x1 + x2 + x3 + x4)
+summary(fit_2)
+shapiro.test(fit_2$residuals)
+#gaussian
+
+vif(fit_2)
+#no problem of collinearity
+
+fit_2_red = lm( response ~ x1 + x3 + x4)
+summary(fit_2_red)
+
+fit_2_red_2 = lm(response ~ x3 + x4)
+summary(fit_2_red_2)
+#it is a model based on the interaction with rolling stock and all the others
+#problema: R^2 bassissimo
+
+
+pc = princomp(trasf_cause)
+pc
+summary(pc)
+finestra_grafica(t)
+plot(pc)
+pc$Loadings
+pc$scores
+biplot(pc)  
+#la differenza tra le teste degli archi rappresenta log-ratio tra le due componenti:
+# più sono diversi, più è grande, e dunque il loro rapporto è tanto più >1
+#Penso che sia meglio fare il modello con la trasformazione piuttosto che con la PCA
+
+#ora provo a fare un gam, per 
+model_gam=gam(response ~ s(x1,bs='cr') + s(x2,bs='cr') + s(x3,bs='cr') + s(x4,bs='cr') )
+summary(model_gam)
+#it seems to explaining it better
+shapiro.test(model_gam$residuals)
+#I have gaussianity
+
+#since I have already seen that there is linearity between response and x3 and x4:
+# I try the semiparametric one to see if it is better
+model_semiparam = gam(response ~ s(x1,bs='cr') + s(x2,bs='cr') + x3 + x4 )
+summary(model_semiparam)
+#R^2 is not better anymore: I try the ANOVA test
+shapiro.test(model_semiparam$residuals)
+#I can do it parametric: it is more powerful
+
+anova(model_semiparam,model_gam, test = "F") 
+#the pavalue is 0.02955: I have to reject H0: I have to retain the nonparametric model
+#but I try to reduce it: I can do it since I have gaussianity
+model_gam_red=gam(response ~ s(x2,bs='cr') + s(x3,bs='cr') + s(x4,bs='cr') )
+summary(model_gam_red)
+#ANOVA test:
+anova(model_gam_red,model_gam, test = "F") 
+#pvalue: 0.5846: I can use the reduced: so it seems that the first one(related to rail infrastrcuture, does not influence anything)
+
+model = model_gam_red
+summary(model)
+
+bounds_x1 = c(min(x1),max(x1))
+bounds_x2 = c(min(x2),max(x2))
+bounds_x3 = c(min(x3),max(x3))
+bounds_x4 = c(min(x4),max(x4))
+#I try maybe something not very cool: prendo questi bounds e faccio sampling 
+# con le uniformi su questi bounds
+install.packages('lhs')
+library(lhs)
+
+sample1 = randomLHS(1000,3)
+sample1_gam_red = data.frame(bounds_x2[1] + (bounds_x2[2]-bounds_x2[1])*sample1[,1],
+                        bounds_x3[1] + (bounds_x3[2]-bounds_x3[1])*sample1[,2],
+                        bounds_x4[1] + (bounds_x4[2]-bounds_x4[1])*sample1[,3]
+                        )
+colnames(sample1_gam_red)=c('x2','x3','x4')
+sample2 = randomLHS(1000,3)
+sample2_gam_red = data.frame(bounds_x2[1] + (bounds_x2[2]-bounds_x2[1])*sample2[,1],
+                        bounds_x3[1] + (bounds_x3[2]-bounds_x3[1])*sample2[,2],
+                        bounds_x4[1] + (bounds_x4[2]-bounds_x4[1])*sample2[,3]
+                        )
+colnames(sample2_gam_red)=c('x2','x3','x4')
+
+S <- sobol(model = model_gam_red, X1 = sample1_gam_red, X2 = sample2_gam_red, order = 1, nboot = 100)
+print(S)
+
+finestra_grafica(t)
+ggplot(S)
+#x3 seems to be the most relevant features: the introduction of the rolling stock in the 
+#variables made it increase
+
+#for the sake of curiosity: I run it on the complete gam
+sample1 = randomLHS(1000,4)
+sample1_gam = data.frame(    bounds_x1[1] + (bounds_x1[2]-bounds_x1[1])*sample1[,1],
+                             bounds_x2[1] + (bounds_x2[2]-bounds_x2[1])*sample1[,2],
+                             bounds_x3[1] + (bounds_x3[2]-bounds_x3[1])*sample1[,3],
+                             bounds_x4[1] + (bounds_x4[2]-bounds_x4[1])*sample1[,4]
+)
+colnames(sample1_gam)=c('x1','x2','x3','x4')
+sample2 = randomLHS(1000,4)
+sample2_gam = data.frame(bounds_x1[1] + (bounds_x1[2]-bounds_x1[1])*sample2[,1],
+                         bounds_x2[1] + (bounds_x2[2]-bounds_x2[1])*sample2[,2],
+                         bounds_x3[1] + (bounds_x3[2]-bounds_x3[1])*sample2[,3],
+                         bounds_x4[1] + (bounds_x4[2]-bounds_x4[1])*sample2[,4]
+)
+colnames(sample2_gam)=c('x1','x2','x3','x4')
+
+S_gam <- sobol(model = model_gam, X1 = sample1_gam, X2 = sample2_gam, order = 1, nboot = 100)
+print(S_gam)
+
+finestra_grafica(S_gam)
+ggplot(S)
+
+#it demonstrates what we have seen until now: rail infrastructure is not so important.
+#Taking account of the rolling stock increases it
+
+#TODO: taking account of the variables one at the time as regressor
